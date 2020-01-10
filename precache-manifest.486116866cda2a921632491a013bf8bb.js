3d2d92518dd260e5f4a4eff1b76b284445e749b0
nguyenchiemhao <nguyenchiemhao2205@gmail.com>
2020-01-10 11:32:46 +0700

    Updates

---
+++ precache-manifest.486116866cda2a921632491a013bf8bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2157e136d2030ff1843672e87300605c",
+    "url": "/HaoMonstersRolodex/index.html"
+  },
+  {
+    "revision": "01cfe3345ead1fc2c315",
+    "url": "/HaoMonstersRolodex/static/css/main.263dbb6e.chunk.css"
+  },
+  {
+    "revision": "a96d3763b474e20d799f",
+    "url": "/HaoMonstersRolodex/static/js/2.fd5481b5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/HaoMonstersRolodex/static/js/2.fd5481b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01cfe3345ead1fc2c315",
+    "url": "/HaoMonstersRolodex/static/js/main.831679d7.chunk.js"
+  },
+  {
+    "revision": "69905f7e580d1f272091",
+    "url": "/HaoMonstersRolodex/static/js/runtime-main.ae4adf73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HaoMonstersRolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
